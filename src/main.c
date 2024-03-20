3ddcbf1d4958eb7463a5706aa0b9cca831ef496c
FedeBurgos <federico.burgos@mi.unc.edu.ar>
2024-03-20 20:28:52 -0300

    CL:
    - Add executable and directory structure

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include "greetings.h"
+#include <stdio.h>
+
+/**
+ * @brief main function for testing configuration.
+ * @param argc: number of arguments
+ * @param argv: array of arguments
+ * @return 1 if succes, 0 if failure
+ * @see greetings
+ * */
+int main(int argc, char* argv[])
+{
+    printf("Hello, OS II. Welcome to 2024 course!\n");
+    greetings();
+    return 0;
+}
